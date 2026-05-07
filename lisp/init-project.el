717c19c2079a70be6e6276f8a7a7459723cc0a29
Simon Guest <simon.guest@tesujimath.org>
2026-05-07 12:56:28 +1200

    Explicitly set project-switch-commands so we get magit

--- lisp/init-project.el
+++ lisp/init-project.el
@@ -6,7 +6,14 @@
 
 (use-package project
   :ensure nil
-  :bind-keymap ("C-c p" . project-prefix-map))
+  :bind-keymap ("C-c p" . project-prefix-map)
+  :custom
+  (project-switch-commands
+   '((project-find-file "Find file" ?f)
+     (project-find-regexp "Find regexp" ?g)
+     (project-dired "Dired" ?d)
+     ;; (project-eshell "Eshell" ?e)
+     (magit-project-status "Magit" ?m))))
 
 (provide 'init-project)
 ;;; init-project.el ends here
