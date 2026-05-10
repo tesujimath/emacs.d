c730108b94164e5496dc7b4a240008675ba2b598
Simon Guest <simon.guest@tesujimath.org>
2026-05-10 15:43:04 +1200

    Add spell-checking with jinx
    Installed jinx binary wit Nix Home Manager

--- lisp/init-text.el
+++ lisp/init-text.el
@@ -6,6 +6,19 @@
 
 (add-hook 'text-mode-hook #'visual-line-mode)
 
+(use-package jinx
+  :ensure nil ; loaded via Nix Home Manager
+  :hook ((text-mode . jinx-mode)
+         (markdown-mode . jinx-mode)
+         (org-mode  . jinx-mode))
+  :bind (("M-$"   . jinx-correct)
+         ("C-M-$" . jinx-languages)
+         :map jinx-mode-map
+         ("M-n" . jinx-next)
+         ("M-p" . jinx-previous))
+  :custom
+  (jinx-languages "en_GB"))
+
 ;; text-oriented modes
 (require 'init-text-beancount)
 (require 'init-text-csv)
