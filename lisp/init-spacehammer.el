2e99e8f362927da45cc059646fd30096e9f4c842
Simon Guest <simon.guest@tesujimath.org>
2026-06-17 17:52:11 +1200

    Add Spacehammer

---
+++ lisp/init-spacehammer.el
@@ -0,0 +1,13 @@
+;;; init-spacehammer --- setup Spacehammer  -*- lexical-binding: t -*-
+
+;;; Commentary:
+
+;;; Code:
+
+(use-package spacehammer
+  :ensure (:host github
+                 :repo "agzam/spacehammer"
+                 :files ("*.el")))
+
+(provide 'init-spacehammer)
+;;; init-spacehammer.el ends here
