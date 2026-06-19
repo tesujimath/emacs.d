6866221904f5ec946960bf4837fc91d4a3fda453
Simon Guest <simon.guest@tesujimath.org>
2026-06-19 14:33:00 +1200

    F# enhancements

--- lisp/init-prog-fsharp.el
+++ lisp/init-prog-fsharp.el
@@ -5,8 +5,16 @@
 ;;; Code:
 
 (use-package fsharp-ts-mode
-  :ensure (:host github
-                 :repo "bbatsov/fsharp-ts-mode"))
+  :ensure (:host github :repo "bbatsov/fsharp-ts-mode")
+  :config
+  (add-to-list 'eglot-server-programs '(fsharp-ts-mode . ("fsautocomplete")))
+  :hook ((fsharp-ts-mode . eglot-ensure)
+         (fsharp-ts-mode . fsharp-ts-repl-minor-mode)))
+
+(with-eval-after-load 'apheleia
+  (setf (alist-get 'fsharp-ts-mode apheleia-mode-alist) 'fantomas)
+  ;; F# autoformat using fantomas
+  (setf (alist-get 'fantomas apheleia-formatters) '("dotnet" "fantomas" input "--out" output)))
 
 (provide 'init-prog-fsharp)
 ;;; init-prog-fsharp.el ends here
