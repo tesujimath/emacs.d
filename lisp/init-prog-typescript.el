004f49959d59da0569dea38ab66585116b5c9144
Simon Guest <simon.guest@tesujimath.org>
2026-07-10 11:32:47 +1200

    Define deno-mode for standalone scripts

--- lisp/init-prog-typescript.el
+++ lisp/init-prog-typescript.el
@@ -1,6 +1,11 @@
 ;;; init-prog-typescript --- setup TypeScript  -*- lexical-binding: t -*-
 
 ;;; Commentary:
+;;;
+;;; We define a deno-mode, derived from typescript-ts-mode, purely for standalone deno scripts,
+;;; because the TypeScript LSP doesn't understand deno-style inline package dependencies.
+;;; Also, at the time of writing, the deno LSP is not as robust as the TypeScript one, so we
+;;; don't want to use it more widely.  This may change in time.
 
 ;;; Code:
 
@@ -8,9 +13,15 @@
   :ensure nil
   :hook ((typescript-ts-mode . eglot-ensure)))
 
+(define-derived-mode deno-mode typescript-ts-mode "Deno"
+  "Major mode for standalone Deno scripts, derived from `typescript-ts-mode'.")
+
+(add-to-list 'interpreter-mode-alist '("deno" . deno-mode))
+
 (with-eval-after-load 'apheleia
   (setf (alist-get 'typescript-mode apheleia-mode-alist) 'biome)
   (setf (alist-get 'typescript-ts-mode apheleia-mode-alist) 'biome)
+  (setf (alist-get 'deno-mode apheleia-mode-alist) 'biome)
   (setf (alist-get 'biome apheleia-formatters) '("biome" "format" "--stdin-file-path" filepath)))
 
 (with-eval-after-load 'eglot
@@ -18,10 +29,18 @@
                '((typescript-ts-mode :language-id "typescript") .
                  ;; multiplexed LSP:
                  ("rass" "tsbiome")
+                 ;; separate LSPs for testing:
+                 ;; ("typescript-language-server" "--stdio")
+                 ;; ("biome" "lsp-proxy")
+                 ))
+  (add-to-list 'eglot-server-programs
+               '((deno-mode :language-id "typescript") .
+                 ;; multiplexed LSP:
                  ;;(eglot-deno-biome "rass" "deno-biome")
                  ;; separate LSPs for testing:
-                 ;;(eglot-deno "deno" "lsp" "-L" "info")
-                 ;; ("biome" "lsp-proxy")
+                 ;; Alas the deno LSP simply doesn't work 😭
+                 ;; (eglot-deno "deno" "lsp" "-L" "info")
+                 ("biome" "lsp-proxy")
                  ))
 
   (defclass eglot-deno-biome (eglot-lsp-server) ()
