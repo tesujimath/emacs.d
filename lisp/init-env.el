08a6fefa3a121d243255a9201e1fa16138f9a1b0
Simon Guest <simon.guest@datacom.com>
2026-08-24 10:42:18 +1200

    Identity more candidates for exec-path-from-shell-variables

--- lisp/init-env.el
+++ lisp/init-env.el
@@ -10,7 +10,10 @@
   (when (memq window-system '(mac ns x))
     (setq exec-path-from-shell-variables
           ;; we need both of these to avoid path reset from nrepl server
-          '("PATH" "__NIX_DARWIN_SET_ENVIRONMENT_DONE"))
+          '("PATH" "__NIX_DARWIN_SET_ENVIRONMENT_DONE"
+            ;; these from exec-path-from-shell README:
+            ;; "SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO" "LANG" "LC_CTYPE" "NIX_SSL_CERT_FILE" "NIX_PATH"
+            ))
     (exec-path-from-shell-initialize))
   )
 
