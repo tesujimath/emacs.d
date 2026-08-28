c8113e20caeb57d151fb5e9082ea387d0c23187e
Simon Guest <simon.guest@datacom.com>
2026-08-28 16:42:56 +1200

    agent-shell: C-c C-f binding;  cursor-agent

--- lisp/init-ai-agent-shell.el
+++ lisp/init-ai-agent-shell.el
@@ -5,10 +5,13 @@
 ;;; Code:
 
 (use-package agent-shell
-  :config
+  :custom
   ;; https://github.com/xenodium/agent-shell#anthropic-claude
-  (setq agent-shell-anthropic-authentication
-        (agent-shell-anthropic-make-authentication :login t)))
+  (agent-shell-anthropic-authentication (agent-shell-anthropic-make-authentication :login t))
+  (agent-shell-cursor-acp-command '("cursor-agent" "acp"))
+
+  :bind (:map agent-shell-mode-map
+              ("C-c C-f" . agent-shell-prompt-compose)))
 
 (provide 'init-ai-agent-shell)
 ;;; init-ai-agent-shell.el ends here
